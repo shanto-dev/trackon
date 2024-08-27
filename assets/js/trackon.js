cf3c43fac7c102826392f31ada96eb0797ef899d
shanto-dev <shantoroy1792@gmail.com>
2024-08-27 21:19:34 +0600

    Update Code on home page

--- assets/js/trackon.js
+++ assets/js/trackon.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
-   
+    $(window).on("scroll", function(){
+        var scrollPosition = $(this).scrollTop();
+
+        if ( scrollPosition > 250 ) {
+            $(".main_header").addClass("trk_header_sticky");
+        } else {
+            $(".main_header").removeClass("trk_header_sticky");
+        }
+    });
+    var show_btn=$('#myModal');
+    //$("#testmodal").modal('show');
+    
+      show_btn.click(function(){
+        $("#myInput").modal('show');
+    })
 })
 window.addEventListener("scroll", function () {
     let scrollPosition = window.pageYOffset;
@@ -17,4 +31,18 @@ window.addEventListener("scroll", function () {
         let reviewOffset = revParallax.offsetTop;
         revParallax.style.backgroundPositionY = (scrollPosition - reviewOffset) * 0.7 + "px";
     }
+    // Service section parallax
+    const servParallax = document.getElementById("serviceTwo_prallax");
+    if (revParallax) {
+        let reviewOffset = servParallax.offsetTop;
+        servParallax.style.backgroundPositionY = (scrollPosition - reviewOffset) * 0.5 + "px";
+    }
+    
+    // Lookbook section parallax
+    const slkbkParallax = document.getElementById("lookbookOne_prallax");
+    if (revParallax) {
+        let reviewOffset = slkbkParallax.offsetTop;
+        slkbkParallax.style.backgroundPositionY = (scrollPosition - reviewOffset) * 0.5 + "px";
+    }
+    
 });
