495f4d7c9c5875e9aa16c8d315cbdc835ecc3184
shanto-dev <shantoroy1792@gmail.com>
2024-08-29 00:20:21 +0600

    update Some Home code

--- assets/js/trackon.js
+++ assets/js/trackon.js
@@ -1,20 +1,45 @@
 $(document).ready(function () {
-    $(window).on("scroll", function(){
+    $(window).on("scroll", function () {
         var scrollPosition = $(this).scrollTop();
 
-        if ( scrollPosition > 250 ) {
+        if (scrollPosition > 250) {
             $(".main_header").addClass("trk_header_sticky");
         } else {
             $(".main_header").removeClass("trk_header_sticky");
         }
     });
-    var show_btn=$('#myModal');
+    var show_btn = $('#myModal');
     //$("#testmodal").modal('show');
-    
-      show_btn.click(function(){
+
+    show_btn.click(function () {
         $("#myInput").modal('show');
-    })
+    });
+
+    // Shuffle 
+    $(window).on('load', function () {
+        if ($(".shuffle_wrap").length > 0) {
+            var Shuffle = window.Shuffle;
+            var $grid = new Shuffle(document.querySelector('.shuffle_wrap'), {
+                itemSelector: '.shuf_item',
+                sizer: '.shuf_sizer',
+            });
+
+            $('.filter_nav li').on('click', function () {
+                $('.filter_nav li').removeClass('active');
+                $(this).addClass('active');
+                var groupName = $(this).attr('data-group');
+                $grid.filter(groupName);
+            });
+        }
+    });
 })
+
+
+
+
+
+
+// scroll Prallax 
 window.addEventListener("scroll", function () {
     let scrollPosition = window.pageYOffset;
 
@@ -37,12 +62,11 @@ window.addEventListener("scroll", function () {
         let reviewOffset = servParallax.offsetTop;
         servParallax.style.backgroundPositionY = (scrollPosition - reviewOffset) * 0.5 + "px";
     }
-    
+
     // Lookbook section parallax
     const slkbkParallax = document.getElementById("lookbookOne_prallax");
     if (revParallax) {
         let reviewOffset = slkbkParallax.offsetTop;
         slkbkParallax.style.backgroundPositionY = (scrollPosition - reviewOffset) * 0.5 + "px";
     }
-    
 });
